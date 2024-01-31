29d4926720034860b8cab8e106bc0d36f743546f
sajipoochira <sajipoochira@gmail.com>
2024-01-31 09:25:23 +0300

    k8s-added

--- ekart/ekart/settings.py
+++ ekart/ekart/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': 5432, #default port you don't need to mention in docker-compose
      
     }
