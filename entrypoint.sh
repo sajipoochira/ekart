f3ea5236f609eefda82f6690d04dbd18da6522d2
sajipoochira <sajipoochira@gmail.com>
2024-02-18 11:50:01 +0300

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ python manage.py migrate --no-input
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('Admin', 'admin@example.com', 'pass')" | python manage.py shell
 
-exit()
-
 python manage.py collectstatic
 
 gunicorn ekart.wsgi:application --bind 0.0.0.0:8000
